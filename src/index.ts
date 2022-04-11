4ce7f8e012dffbedfdcdb597137376e5ddd430d6
jaceksanko <jacek.sanko@gmail.com>
2022-04-11 21:34:16 +0200

    Comment add test user

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import { Request, Response } from 'express';
 import { AppDataSource } from './data-source';
-import { User, UserRole } from './entity/User';
 import { Routes } from './routes';
 
 AppDataSource.initialize()
@@ -15,20 +14,16 @@ AppDataSource.initialize()
     Routes.forEach((route) => {
       (app as any)[route.method](
         route.route,
-        (req: Request, res: Response, next: Function) => {
-          const result = new (route.controller as any)()[route.action](
-            req,
-            res,
-            next
-          );
-          if (result instanceof Promise) {
-            result.then((result) =>
-              result !== null && result !== undefined
-                ? res.send(result)
-                : undefined
+        async (req: Request, res: Response, next: Function) => {
+          try {
+            const result = await new (route.controller as any)()[route.action](
+              req,
+              res,
+              next
             );
-          } else if (result !== null && result !== undefined) {
             res.json(result);
+          } catch (err) {
+            next(err);
           }
         }
       );
@@ -41,15 +36,15 @@ AppDataSource.initialize()
     app.listen(3000);
 
     // insert new users for test
-    await AppDataSource.manager.save(
-      AppDataSource.manager.create(User, {
-        firstName: 'Timber',
-        lastName: 'Saw',
-        email: 'test@test.com',
-        password: '123',
-        role: UserRole.ADMIN,
-      })
-    );
+    // await AppDataSource.manager.save(
+    //   AppDataSource.manager.create(User, {
+    //     firstName: 'Timber',
+    //     lastName: 'Saw',
+    //     email: 'test@test.com',
+    //     password: '123',
+    //     role: UserRole.ADMIN,
+    //   })
+    // );
 
     console.log(
       'Express server has started on port 3000. Open http://localhost:3000/users to see results'
